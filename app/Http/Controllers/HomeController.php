028849ce1980b5b21b21b1ed9767057675ab80d2
kongkiat.b <kongkiat.0174@gmail.com>
2022-09-19 10:37:01 +0700

    TestKong

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        // dd($user);
+        return view('test.index',[
+            'name'  => $user->name
+        ]);
+    }
+}
